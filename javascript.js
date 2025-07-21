9478af7f05eedf001475c5f0c02b5ca367ab3d6c
lordcashflow <45726660+lordcashflow@users.noreply.github.com>
2025-07-21 21:14:30 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('#slider .slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Change slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
